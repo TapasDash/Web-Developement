323a8163e8cc062db251009dd2c36d73c45070af
TapasDash <tapasdash017@gmail.com>
2020-05-12 02:24:46 +0530

    Create edit.js

---
+++ NodeJS/edit.js
@@ -0,0 +1,28 @@
+/*
+
+*/
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'progate',
+  password: 'password',
+  database: 'list_app'
+});
+
+app.get('/edit/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.render('edit.ejs', {item: results[0]});
+    }
+  );
+});
+
+app.listen(3000);
