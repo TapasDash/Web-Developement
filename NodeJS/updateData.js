9c21f723d4cac794aceded9d209a2cda093cc865
TapasDash <tapasdash017@gmail.com>
2020-05-12 02:47:20 +0530

    Create updateData.js

---
+++ NodeJS/updateData.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'progate',
+  password: 'password',
+  database: 'list_app'
+});
+
+app.post('/update/:id', (req, res) => {
+  // Write code to update the selected item
+  connection.query(
+    'UPDATE items SET name = ? WHERE id = ?',
+    [req.body.itemName, req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    });
+});
+
+app.listen(3000);
