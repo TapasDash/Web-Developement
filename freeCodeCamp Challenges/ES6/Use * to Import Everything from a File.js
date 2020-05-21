bcfb947717cd83162606f250bb8c54353155f332
TapasDash <tapasdash017@gmail.com>
2020-05-21 06:36:40 +0530

    Create Use * to Import Everything from a File.js

---
+++ freeCodeCamp Challenges/ES6/Use * to Import Everything from a File.js
@@ -0,0 +1,13 @@
+/* string_functions.js 
+export const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+export const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+*/
+
+import * as stringFunctions from "./string_functions.js";
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
