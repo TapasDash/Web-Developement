d3cee0a877d041ec21bef364f1827708b71f9e2f
TapasDash <tapasdash017@gmail.com>
2020-05-21 06:44:29 +0530

    Create Create an Export Fallback with export default.js

---
+++ freeCodeCamp Challenges/ES6/Create an Export Fallback with export default.js
@@ -0,0 +1,9 @@
+/*
+Since export default is used to declare a fallback value for a module or file, 
+you can only have one value be a default export in each module or file.
+Additionally, you cannot use export default with var, let, or const
+*/
+
+export default function subtract(x, y) {
+  return x - y;
+}
