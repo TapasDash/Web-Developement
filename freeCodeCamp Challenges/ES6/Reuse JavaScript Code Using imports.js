519c58758ea7a2c14c98f7303949734c3ba646e9
TapasDash <tapasdash017@gmail.com>
2020-05-21 06:30:58 +0530

    Create Reuse JavaScript Code Using imports.js

---
+++ freeCodeCamp Challenges/ES6/Reuse JavaScript Code Using imports.js
@@ -0,0 +1,15 @@
+/* string_functions.js 
+
+export const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+export const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+*/
+
+import { uppercaseString, lowercaseString } from './string_functions.js';
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
