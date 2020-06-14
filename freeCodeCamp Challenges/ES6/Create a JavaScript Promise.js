03c72890c2fa96c66dcb90b51b2c688b32d700a3
TapasDash <tapasdash017@gmail.com>
2020-06-14 13:18:26 +0530

    Create Create a JavaScript Promise.js

---
+++ freeCodeCamp Challenges/ES6/Create a JavaScript Promise.js
@@ -0,0 +1,6 @@
+//A promise in JavaScript is exactly what it sounds like - you use it to make a promise to do something, usually asynchronously.
+//When the task completes, you either fulfill your promise or fail to do so
+
+const makeServerRequest = new Promise((resolve,reject) => {
+  
+});
