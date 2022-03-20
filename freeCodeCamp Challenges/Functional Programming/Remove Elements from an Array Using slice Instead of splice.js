3d5ded927dfe35c89de3a1f68254d0691815835e
TapasDash <tapasdash017@gmail.com>
2022-03-20 17:29:55 +0530

    Create Remove Elements from an Array Using slice Instead of splice.js

---
+++ freeCodeCamp Challenges/Functional Programming/Remove Elements from an Array Using slice Instead of splice.js
@@ -0,0 +1,10 @@
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  // console.log( cities.slice(3)); [ 'London', 'Berlin' ]
+  // console.log( cities.slice(2)); [ 'Islamabad', 'London', 'Berlin' ]
+return cities.slice(0,3)
+  // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities); //
