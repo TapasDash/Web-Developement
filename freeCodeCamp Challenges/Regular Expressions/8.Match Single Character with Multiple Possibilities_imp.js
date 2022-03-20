162f755ca84921e0c0184c2f16308035fafb165e
TapasDash <tapasdash017@gmail.com>
2022-03-21 02:16:38 +0530

    Create 8.Match Single Character with Multiple Possibilities_imp.js

---
+++ freeCodeCamp Challenges/Regular Expressions/8.Match Single Character with Multiple Possibilities_imp.js
@@ -0,0 +1,29 @@
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
+// result = [ 'e',
+//   'a',
+//   'e',
+//   'o',
+//   'u',
+//   'i',
+//   'e',
+//   'a',
+//   'o',
+//   'e',
+//   'o',
+//   'e',
+//   'I',
+//   'a',
+//   'e',
+//   'o',
+//   'o',
+//   'e',
+//   'i',
+//   'o',
+//   'e',
+//   'o',
+//   'i',
+//   'e',
+//   'i' ]
