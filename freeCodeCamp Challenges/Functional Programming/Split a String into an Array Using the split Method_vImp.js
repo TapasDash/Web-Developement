b87bd621d2416e6c8c8ce2ab7ca58dd3c0baaef7
TapasDash <tapasdash017@gmail.com>
2022-03-21 17:13:47 +0530

    Create Split a String into an Array Using the split Method_vImp.js

---
+++ freeCodeCamp Challenges/Functional Programming/Split a String into an Array Using the split Method_vImp.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+
+return str.split(/\W/i)  // [ 'Hello', 'World', 'I', 'am', 'code' ]
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
