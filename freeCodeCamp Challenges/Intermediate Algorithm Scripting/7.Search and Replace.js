085e236b2f4c3f6f4bca369b5abc960fd6e9ade3
TapasDash <tapasdash017@gmail.com>
2022-03-29 23:53:26 +0530

    Create 7.Search and Replace.js

---
+++ freeCodeCamp Challenges/Intermediate Algorithm Scripting/7.Search and Replace.js
@@ -0,0 +1,8 @@
+function myReplace(str, before, after) {
+  const strArr = str.split(" ");
+  const [wordToReplace] = strArr.filter(item => item === before);
+  const replacement = wordToReplace[0] === wordToReplace[0].toUpperCase()
+    ? after[0].toUpperCase() + after.slice(1)
+    : after[0].toLowerCase() + after.slice(1);
+  return strArr.map(item => (item === before ? replacement : item)).join(" ");
+}
